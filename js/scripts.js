ff748c8395f5786441c875c0acf44b2b719df80d
joshjabs <jjabshear@coloradomesa.edu>
2021-09-21 17:00:06 -0600

    worked on some javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,19 @@
+var a = ["test", "out", "arrays"]
+var b = 0
+
+function sum(){
+
+  //up to developer's choice
+  // console.log('starting sum function')
+  // c =  a % b
+  // console.log('finished sum function')
+
+
+}
 
 function giveMessage(){
-
-  alert("Sample alert message")
-  
+  // comment
+  console.log( a[b] )
+  b++
 }
   
